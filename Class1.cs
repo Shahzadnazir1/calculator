c05b7341aa0466a3654c35091c4e23b14e46dcf4
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-06-19 18:11:22 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿class calculator
+{
+    int a, b,
+        a = 10 , b = 20;
+    public int add(int a, int b)
+        public int sub(int a, int b)
+        public int divide(int a, int b)
+        public int multiplication(int a, int b)
+        void Main(string[] args)
+
+
+    {
+
+        return a + b;
+        return  a-b;
+        return a * b;
+        return a / b;
+
+    }
